da7f4b08c9b208cdaa08934f8c0b6aaeb0db8870
Prasanth K S <ksprasanthksp10@gmail.com>
2024-07-31 18:46:24 +0530

    small modification

--- src/components/firstComponent.jsx
+++ src/components/firstComponent.jsx
@@ -3,7 +3,7 @@ import MyClassComponent from "./classComponent";
 var MyFirstComponent = () => {
   return (
     <div>
-      <MyClassComponent className="ecec"></MyClassComponent>
+      <MyClassComponent/>
       <h1 className="ecec">I am in SKCET College</h1>
       <h2 className="ecec">Training ECE C Students on MERN</h2>
     </div>
