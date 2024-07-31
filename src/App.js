08aea9c5d73394d57421cfc34cd65fc3ab57c38c
Prasanth K S <ksprasanthksp10@gmail.com>
2024-07-31 17:04:58 +0530

    React Project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import MyFirstComponent from './components/firstComponent';
 function App() {
   return (
     <div className="App">
+      <MyFirstComponent/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/firstComponent.jsx
@@ -0,0 +1,12 @@
+import "../css/myFirstCss.css";
+import MyClassComponent from "./classComponent";
+var MyFirstComponent = () => {
+  return (
+    <div>
+      <MyClassComponent className="ecec"></MyClassComponent>
+      <h1 className="ecec">I am in SKCET College</h1>
+      <h2 className="ecec">Training ECE C Students on MERN</h2>
+    </div>
+  );
+};
+export default MyFirstComponent;
